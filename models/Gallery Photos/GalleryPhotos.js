1b16c3a27586bf372eaf6dbf44eb86b5c47eed90
Tushya123 <tushyagandhi123@gmail.com>
2024-05-16 11:06:43 +0530

    changes

--- models/Gallery Photos/GalleryPhotos.js
+++ models/Gallery Photos/GalleryPhotos.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const GalleryPhotoSchema = new mongoose.Schema(
   {
     Category: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
       ref:"Category"
     },
     imageURL: {
