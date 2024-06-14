3d4c7225f2f354d5ffc2e91864256ef2e20665e2
MeghaPatel07 <meghap1920@gmail.com>
2024-06-14 11:43:05 +0530

    brochure

--- controllers/ProductDetail/ProductDetail.js
+++ controllers/ProductDetail/ProductDetail.js
@@ -3,6 +3,12 @@ const supplierquote=require("../../models/SupplierQuote/SupplierQuote")
 const PDFDocument = require("pdfkit");
 const path = require('path');
 const fs = require("fs");
+const puppeteer = require('puppeteer'); 
+const handlebars = require('handlebars'); 
+const img1 = path.join(__dirname, 'header.png');
+const ejs = require('ejs');
+const htmlPdf = require('html-pdf');
+
 exports.createProjectDetail = async (req, res) => {
   try {
     if (!fs.existsSync(`${__basedir}/uploads/ProductDetailImages`)) {
@@ -364,168 +370,272 @@ exports.getProductByDescription = async (req, res) => {
 const axios = require('axios');
 
 
+// exports.downloadPdf = async (req, res, next) => {
+//   try {
+//     console.log("req.body", req.body);
+//     const { Description, ImageUrl, ProductDetailDescription } = req.body;
 
+//     // Fetch and save the logo image temporarily
+//     const logoUrl = 'https://front.shreejipharma.in/static/media/logo.eecbf1c37f0a264bcea6.png';
+//     const logoResponse = await axios.get(logoUrl, { responseType: 'arraybuffer' });
+//     const tempLogoPath = path.join(__dirname, `temp-logo-${Date.now()}.jpg`);
+//     fs.writeFileSync(tempLogoPath, logoResponse.data);
 
+//     // Fetch and save the product image temporarily
+//     const productImageUrl = `https://server.shreejipharma.in/${ImageUrl}`;
+//     const productImageResponse = await axios.get(productImageUrl, { responseType: 'arraybuffer' });
+//     const tempProductImagePath = path.join(__dirname, `temp-product-image-${Date.now()}.jpg`);
+//     fs.writeFileSync(tempProductImagePath, productImageResponse.data);
 
+//     // Read and compile the HTML template
+//     const templateHtml = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf8');
+//     const template = handlebars.compile(templateHtml);
+
+//     // Replace placeholders with actual data
+//     const html = template({
+//       logoUrl: `file://${tempLogoPath}`,
+//       Description,
+//       ImageUrl: `file://${tempProductImagePath}`,
+//       ProductDetailDescription,
+//     });
+
+//     // Launch Puppeteer and create the PDF
+//     const browser = await puppeteer.launch({
+//       args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//       headless: true,
+//       timeout: 60000 // Increase timeout to 60 seconds
+//     });
+//     const page = await browser.newPage();
+//     await page.setContent(html, { waitUntil: 'networkidle0', timeout: 60000 });
+//     const pdfBuffer = await page.pdf({ format: 'A4' });
+
+//     await browser.close();
+
+//     // Clean up temporary files
+//     fs.unlinkSync(tempLogoPath);
+//     fs.unlinkSync(tempProductImagePath);
+
+//     // Set response headers and send the PDF
+//     res.setHeader('Content-Type', 'application/pdf');
+//     res.setHeader('Content-Disposition', `attachment; filename="${Description}-${Date.now()}.pdf"`);
+//     res.send(pdfBuffer);
+//   } catch (err) {
+//     console.error('Error generating PDF:', err); // Improved error logging
+//     next(err);
+//   }
+// };
+
+ 
+// exports.downloadPdf = async (req, res, next) => {
+//   try {
+//     const { Description, ImageUrl, ProductDetailDescription } = req.body;
+
+//     const doc = new PDFDocument();
+
+//     // Set response headers to allow CORS
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+//     // Set response headers for PDF content
+//     res.setHeader("Content-Type", "application/pdf");
+//     res.setHeader("Content-Disposition", `attachment; filename="${Description}-${Date.now()}.pdf"`);
+
+//     // Pipe the PDF content to the response
+//     doc.pipe(res);
+//     const logoUrl = 'https://front.shreejipharma.in/static/media/logo.eecbf1c37f0a264bcea6.png'; // Update with the actual logo URL
+
+//     // Download the logo image
+//     const logoResponse = await axios({
+//       url: logoUrl,
+//       responseType: 'arraybuffer'
+//     });
+
+//     const tempLogoPath = path.join(__dirname, `temp-logo-${Date.now()}.jpg`);
+//     fs.writeFileSync(tempLogoPath, logoResponse.data);
+
+//     if (fs.existsSync(tempLogoPath)) {
+//       doc.image(tempLogoPath, {
+//         fit: [100, 100],
+//         align: 'left',
+//       });
+//       fs.unlinkSync(tempLogoPath);
+//     }
+
+//     // Add the bold text
+//     doc.font('Helvetica-Bold').fontSize(20).fillColor('#16436f').text("Shreeji Pharma International", 120, 75, { align: 'right' });
+//     doc.font('Helvetica-Bold').fontSize(11).fillColor('#16436f').text("contact@shreejipharma.com", 120, 105, { align: 'right' });
+//     doc.font('Helvetica-Bold').fontSize(11).fillColor('#16436f').text("+918866002331", 120, 125, { align: 'right' });
+//     doc.moveDown(4);
+  
+//     // Set font size and color for description text and align to the left
+//     doc.font('Helvetica-Bold').fontSize(23).fillColor('#16436f').text(Description, {
+//       align: 'center'
+//     });
+
+//     if (ImageUrl) {
+//       const imageUrl = `https://server.shreejipharma.in/${ImageUrl}`;
+//       console.log("Downloading image from:", imageUrl);
+
+//       const response = await axios({
+//         url: imageUrl,
+//         responseType: 'arraybuffer'
+//       });
+
+//       const tempImagePath = path.join(__dirname, `temp-image-${Date.now()}.jpg`);
+//       fs.writeFileSync(tempImagePath, response.data);
+
+//       if (fs.existsSync(tempImagePath)) {
+//         // Calculate horizontal position to center the image
+//         const maxWidth = doc.page.width;
+//         const imageWidth = maxWidth; // Slightly less than the max width of the page
+
+//         // Calculate horizontal position to center the image
+//         const x = (doc.page.width - imageWidth) / 2;
+
+//         // Calculate vertical position to place the image below the text
+//         const y = doc.y + 15; // Assuming some space between text and image
+
+//         doc.image(tempImagePath, x, y, {
+//           fit: [imageWidth, 300],
+//           align: 'center'
+//         });
+
+//         // Delete the temp image after use
+//         fs.unlinkSync(tempImagePath);
+//       } else {
+//         console.error("Image not found:", tempImagePath);
+//       }
+//     }
+
+//     // Add some space below the image 
+//     doc.moveDown(14);
+
+//     // Add product details in tabular form
+//     // Center align the "Product Details:" text
+//     // doc.fontSize(20).fillColor('#16436f').text("Product Details:", { underline: true, align: 'center' });
+//     // doc.moveDown();
+
+//     // Calculate table dimensions
+//     const tableTop = doc.y;
+//     const tableLeft = 40;
+//     const keyWidth = 200;
+//     const valueWidth = 300;
+//     const rowPadding = 14;
+
+//     // Draw table headers with borders
+//     // (Commented out because they're not being used in this code snippet)
+
+//     // Draw table rows with borders
+//     doc.font('Helvetica-Bold').fontSize(12).fillColor('#16436f')
+//       .text("Name", tableLeft + rowPadding, tableTop + rowPadding, { width: keyWidth - rowPadding * 2 })
+//       .text("Detail", tableLeft + keyWidth + 30 + rowPadding, tableTop + rowPadding, { width: valueWidth - rowPadding * 2 });
+//     doc.moveDown(1);
+//     // Draw borders for headers
+//     const headerHeight = doc.heightOfString("Name", { width: keyWidth - rowPadding * 2 }) + rowPadding * 2;
+//     doc.rect(tableLeft, tableTop, keyWidth, headerHeight).strokeColor('#16436f').stroke();
+//     doc.rect(tableLeft + keyWidth, tableTop, valueWidth + 58, headerHeight).strokeColor('#16436f').stroke();
+
+//     ProductDetailDescription.forEach((detail, index) => {
+//       const keyHeight = doc.heightOfString(detail.ProductKey, {
+//         width: keyWidth - rowPadding * 2,
+//       });
+//       const valueHeight = doc.heightOfString(detail.ProductValue, {
+//         width: valueWidth - rowPadding * 2,
+//       });
+//       const rowHeight = Math.max(keyHeight, valueHeight) + rowPadding * 2;
+
+//       // Check if there is enough space for the next row, if not, add a new page
+//       if (doc.y + rowHeight > doc.page.height - doc.page.margins.bottom) {
+//         doc.addPage();
+//         const newTableTop = doc.y;
+
+//         // Draw table headers with borders on the new page
+//         // (You may include this part if needed)
+
+//         doc.moveDown(2); // Move down after adding a new page
+//       }
+
+//       const y = doc.y;
+
+//       // Draw the Product Key
+//       doc.font('Helvetica').fontSize(12).fillColor('#16436f').text(detail.ProductKey, tableLeft + rowPadding, y + rowPadding, {
+//         width: keyWidth - rowPadding * 2,
+//       });
+
+//       // Draw the Product Value
+//       doc.font('Helvetica').fontSize(12).fillColor('#16436f').text(detail.ProductValue, tableLeft + keyWidth + 30 + rowPadding, y + rowPadding, {
+//         width: valueWidth - rowPadding * 2,
+//       });
+
+//       // Draw borders for the row
+//       doc.rect(tableLeft, y, keyWidth, rowHeight).strokeColor('#16436f').stroke();
+//       doc.rect(tableLeft + keyWidth, y, valueWidth + 58, rowHeight).strokeColor('#16436f').stroke();
+
+//       doc.moveDown(); // Move down after the row
+//     });
+
+//     // End the document
+//     doc.end();
+
+//     // Handle document errors
+//     doc.on('error', (err) => {
+//       next(err);
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 exports.downloadPdf = async (req, res, next) => {
   try {
     const { Description, ImageUrl, ProductDetailDescription } = req.body;
 
-    const doc = new PDFDocument();
+    // Fetch and save the header image temporarily
+    const headerImageUrl = 'https://front.shreejipharma.in/static/media/logo.eecbf1c37f0a264bcea6.png';
+    const headerImageResponse = await axios.get(headerImageUrl, { responseType: 'arraybuffer' });
+    const tempHeaderImagePath = path.join(__dirname, `temp-header-${Date.now()}.png`);
+    fs.writeFileSync(tempHeaderImagePath, headerImageResponse.data);
 
-    // Set response headers to allow CORS
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    // Fetch and save the product image temporarily
+    const productImageUrl = `https://server.shreejipharma.in/${ImageUrl}`;
+    const productImageResponse = await axios.get(productImageUrl, { responseType: 'arraybuffer' });
+    const tempProductImagePath = path.join(__dirname, `temp-product-${Date.now()}.png`);
+    fs.writeFileSync(tempProductImagePath, productImageResponse.data);
 
-    // Set response headers for PDF content
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", `attachment; filename="${Description}-${Date.now()}.pdf"`);
-
-    // Pipe the PDF content to the response
-    doc.pipe(res);
-    const logoUrl = 'https://front.shreejipharma.in/static/media/logo.eecbf1c37f0a264bcea6.png'; // Update with the actual logo URL
-
-    // Download the logo image
-    const logoResponse = await axios({
-      url: logoUrl,
-      responseType: 'arraybuffer'
+    console.log(img1)
+    console.log(`https://server.shreejipharma.in/${ImageUrl}`)
+    console.log(`https://server.shreejipharma.in/controllers/ProductDetail/header.png`)
+    console.log('ProductDetailDescription',ProductDetailDescription)
+    // Render the EJS template to HTML
+    const templatePath = path.join(__dirname, 'brocher.ejs');
+    const html = await ejs.renderFile(templatePath, {
+      headerImage: 'https://server.shreejipharma.in/uploads/header.png',
+      productImage: `https://server.shreejipharma.in/${ImageUrl}`,
+      productName: Description,
+      productDetails: ProductDetailDescription
     });
 
-    const tempLogoPath = path.join(__dirname, `temp-logo-${Date.now()}.jpg`);
-    fs.writeFileSync(tempLogoPath, logoResponse.data);
+    // Convert the rendered HTML to PDF
+    const options = { format: 'A4' };
+    htmlPdf.create(html, options).toStream((err, stream) => {
+      if (err) {
+        return next(err);
+      }
 
-    if (fs.existsSync(tempLogoPath)) {
-      doc.image(tempLogoPath, {
-        fit: [100, 68],
-        align: 'left',
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `attachment; filename="${Description}-${Date.now()}.pdf"`);
+      
+      // Clean up temporary files after sending the response
+      stream.on('end', () => {
+        fs.unlinkSync(tempHeaderImagePath);
+        fs.unlinkSync(tempProductImagePath);
       });
-      fs.unlinkSync(tempLogoPath);
-    }
-    // Add the bold text
-    doc.font('Helvetica-Bold').fontSize(20).fillColor('#16436f').text("Shreeji Pharma International", 120, 75, { align: 'right' });
-    doc.font('Helvetica-Bold').fontSize(11).fillColor('#16436f').text("contact@shreejipharma.com", 120, 105, { align: 'right' });
-    doc.font('Helvetica-Bold').fontSize(11).fillColor('#16436f').text("+918866002331", 120, 125, { align: 'right' });
-    doc.moveDown(4);
-  
-    // Set font size and color for description text and align to the left
-    doc.font('Helvetica-Bold').fontSize(23).fillColor('#16436f').text(Description, {
-      align: 'left'
-    });
 
-if (ImageUrl) {
-  const imageUrl = `https://server.shreejipharma.in/${ImageUrl}`;
-  console.log("Downloading image from:", imageUrl);
-
-  const response = await axios({
-    url: imageUrl,
-    responseType: 'arraybuffer'
-  });
-
-  const tempImagePath = path.join(__dirname, `temp-image-${Date.now()}.jpg`);
-  fs.writeFileSync(tempImagePath, response.data);
-
-  if (fs.existsSync(tempImagePath)) {
-    // Calculate horizontal position to center the image
-    const maxWidth = doc.page.width;
-        const imageWidth = maxWidth; // Slightly less than the max width of the page
-
-        // Calculate horizontal position to center the image
-        const x = (doc.page.width - imageWidth) / 2;
-
-        // Calculate vertical position to place the image below the text
-        const y = doc.y + 15; // Assuming some space between text and image
-
-        doc.image(tempImagePath, x, y, {
-          fit: [imageWidth, 300],
-          align:'center'
-        });
-
-
-    // Delete the temp image after use
-    fs.unlinkSync(tempImagePath);
-  } else {
-    console.error("Image not found:", tempImagePath);
-  }
-}
-
-    // Add some space below the image
-    doc.moveDown(14);
-
-    // Add product details in tabular form
-   // Center align the "Product Details:" text
-// doc.fontSize(20).fillColor('#16436f').text("Product Details:", { underline: true, align: 'center' });
-// doc.moveDown();
-
-// Calculate table dimensions
-const tableTop = doc.y;
-const tableLeft = 40;
-const keyWidth = 200;
-const valueWidth = 300;
-const rowPadding = 14;
-
-// Draw table headers with borders
-// (Commented out because they're not being used in this code snippet)
-
-// Draw table rows with borders
-doc.font('Helvetica-Bold').fontSize(12).fillColor('#16436f')
-.text("Name", tableLeft + rowPadding, tableTop + rowPadding, { width: keyWidth - rowPadding * 2 })
-.text("Detail", tableLeft + keyWidth + 30 + rowPadding, tableTop + rowPadding, { width: valueWidth - rowPadding * 2 });
-doc.moveDown(1)
-// Draw borders for headers
-const headerHeight = doc.heightOfString("Name", { width: keyWidth - rowPadding * 2 }) + rowPadding * 2;
-doc.rect(tableLeft, tableTop, keyWidth, headerHeight).strokeColor('#16436f').stroke();
-doc.rect(tableLeft + keyWidth, tableTop, valueWidth + 58, headerHeight).strokeColor('#16436f').stroke();
-
-ProductDetailDescription.forEach((detail, index) => {
-  const keyHeight = doc.heightOfString(detail.ProductKey, {
-    width: keyWidth - rowPadding * 2,
-  });
-  const valueHeight = doc.heightOfString(detail.ProductValue, {
-    width: valueWidth - rowPadding * 2,
-  });
-  const rowHeight = Math.max(keyHeight, valueHeight) + rowPadding * 2;
-
-  // Check if there is enough space for the next row, if not, add a new page
-  if (doc.y + rowHeight > doc.page.height - doc.page.margins.bottom) {
-    doc.addPage();
-    const newTableTop = doc.y;
-
-    // Draw table headers with borders on the new page
-    // (You may include this part if needed)
-
-    doc.moveDown(2); // Move down after adding a new page
-  }
-
-  const y = doc.y;
-
-  // Draw the Product Key
-  doc.font('Helvetica').fontSize(12).fillColor('#16436f').text(detail.ProductKey, tableLeft + rowPadding, y + rowPadding, {
-    width: keyWidth - rowPadding * 2,
-    
-  });
-
-  // Draw the Product Value
-  doc.font('Helvetica').fontSize(12).fillColor('#16436f').text(detail.ProductValue, tableLeft + keyWidth + 30 + rowPadding, y + rowPadding, {
-    width: valueWidth - rowPadding * 2,
-    
-  });
-
-  // Draw borders for the row
-  doc.rect(tableLeft, y, keyWidth, rowHeight).strokeColor('#16436f').stroke();
-  doc.rect(tableLeft + keyWidth, y, valueWidth+58, rowHeight).strokeColor('#16436f').stroke();
-
-  doc.moveDown(); // Move down after the row
-});
-
-    // End the document
-    doc.end();
-
-    // Handle document errors
-    doc.on('error', (err) => {
-      next(err);
+      // Pipe the PDF stream to the response
+      stream.pipe(res);
     });
   } catch (err) {
+    console.error('Error generating PDF:', err);
     next(err);
   }
 };
