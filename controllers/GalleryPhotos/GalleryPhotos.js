d3dc8fc26865d1b5c30d0eea7414f103d0652a3f
Devanshi Patel <devanshipatel2611@gmail.com>
2024-07-03 11:24:54 +0530

    gallery-photos-3.7.24

--- controllers/GalleryPhotos/GalleryPhotos.js
+++ controllers/GalleryPhotos/GalleryPhotos.js
@@ -1,18 +1,36 @@
 const GalleryPhotoSchema = require("../../models/Gallery Photos/GalleryPhotos");
+const sharp = require("sharp"); // Import the sharp library
+const fs = require("fs").promises; // Import the 'fs.promises' module
+
 exports.createGalleryPhoto = async (req, res) => {
-  
   try {
-    let imageURL = req.file
-    ? `uploads/GalleryPhoto/${req.file.filename}`
-      : null;
+    // let imageURL = req.file
+    // ? `uploads/GalleryPhoto/${req.file.filename}`
+    //   : null;
+    let imageURL = req.file ? req.file : null;
+    console.log("cc", imageURL);
     let { Category, IsActive } = req.body;
 
-    console.log("rsrsrsrsrsrsrs",imageURL);
+    if (imageURL) {
+      const tempResizedImageCP = `uploads/GalleryPhoto/tempGP_${imageURL.filename}`;
+      const PATH = imageURL.path;
+      await sharp(PATH)
+        .resize({
+          width: 500,
+          height: 500,
+          fit: "contain",
+          background: "white",
+        })
+        .toFile(tempResizedImageCP);
+
+      await fs.unlink(PATH);
+      await fs.rename(tempResizedImageCP, PATH);
+    }
 
     const newProject = await new GalleryPhotoSchema({
-        Category,
-      IsActive,
-      imageURL
+      Category: Category,
+      IsActive: IsActive,
+      imageURL: imageURL.path,
     }).save();
 
     res.status(200).json({
@@ -24,62 +42,79 @@ exports.createGalleryPhoto = async (req, res) => {
     console.error(err);
     res.status(500).json({ isOk: false, error: "Internal server error" });
   }
-
 };
 
 exports.listGalleryPhotos = async (req, res) => {
   try {
     const list = await GalleryPhotoSchema.aggregate([
-        {
-          $lookup: {
-            from: 'categories',
-            localField: 'Category', 
-            foreignField: '_id',  
-            as: 'GalleryTypeDetails'
-          }
+      {
+        $lookup: {
+          from: "categories",
+          localField: "Category",
+          foreignField: "_id",
+          as: "GalleryTypeDetails",
         },
-        {
-          $sort: { createdAt: -1 }
-        }
-      ]);
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+    ]);
     res.json(list);
   } catch (error) {
     return res.status(400).send(error);
   }
 };
 
-
 exports.updateGalleryPhotos = async (req, res) => {
-    try {
-        console.log("kokokkokokokokoko",req.file)
-        let imageURL = req.file
-        ? `uploads/GalleryPhoto/${req.file.filename}`
-          : req.body.imageURL;
-        let { Category,IsActive} = req.body;
-    
-        console.log("rsrsrsrsrsrsrs",imageURL);
+  try {
+    let imageURL = req.file
+      ? `uploads/GalleryPhoto/${req.file.filename}`
+      : req.body.imageURL;
+    let { Category, IsActive } = req.body;
 
-        const update = await GalleryPhotoSchema.findOneAndUpdate(
-            { _id: req.params },
-            { $set: { 
-                "Category": Category,
-                "IsActive": IsActive,
-                
-                "imageURL": imageURL
+    console.log("rsrsrsrsrsrsrs", imageURL);
 
-                 } },
-            { new: true }
-          );
-    
-        res.status(200).json({
-          isOk: true,
-          data: update,
-          message: "Project updated successfully",
-        });
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ isOk: false, error: "Internal server error" });
-      }
+    if (imageURL) {
+      // Create a temporary file path for the resized image
+      const tempResizedImageCP = `uploads/GalleryPhoto/tempGP_${imageURL.filename}`;
+      // const PATH = ProductImage.path;
+
+      await sharp(imageURL)
+        .resize({
+          width: 500,
+          height: 500,
+          fit: "contain",
+          background: "white", // Set background color to white
+        })
+        .toFile(tempResizedImageCP);
+
+      // Remove the original image
+      await fs.unlink(imageURL);
+      await fs.rename(tempResizedImageCP, imageURL);
+    }
+
+    const update = await GalleryPhotoSchema.findOneAndUpdate(
+      { _id: req.params },
+      {
+        $set: {
+          Category: Category,
+          IsActive: IsActive,
+
+          imageURL: imageURL,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      isOk: true,
+      data: update,
+      message: "Project updated successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ isOk: false, error: "Internal server error" });
+  }
 };
 
 exports.removeGalleryPhotos = async (req, res) => {
@@ -93,15 +128,12 @@ exports.removeGalleryPhotos = async (req, res) => {
   }
 };
 
-
-
 exports.listGalleryPhotosByParams = async (req, res) => {
   try {
     let { skip, per_page, sorton, sortdir, match, IsActive } = req.body;
     console.log("Received skip:", skip);
     console.log("Received per_page:", per_page);
     console.log("Received IsActive:", IsActive);
-
 
     let query = [
       {
@@ -129,7 +161,7 @@ exports.listGalleryPhotosByParams = async (req, res) => {
             },
             {
               Description: new RegExp(match, "i"),
-            }
+            },
           ],
         },
       },
@@ -167,13 +199,11 @@ exports.listGalleryPhotosByParams = async (req, res) => {
         $project: {
           count: "$stage1.count",
           data: "$stage2",
-        //   GalleryTypeDetails: { $arrayElemAt: ["$GalleryTypeDetails", 0] },
-
+          //   GalleryTypeDetails: { $arrayElemAt: ["$GalleryTypeDetails", 0] },
         },
       },
     ];
 
-   
     if (sorton && sortdir) {
       let sort = {};
       sort[sorton] = sortdir == "desc" ? -1 : 1;
@@ -196,14 +226,11 @@ exports.listGalleryPhotosByParams = async (req, res) => {
   }
 };
 
-exports.getspecificGalleryPhoto=async(req,res)=>{
-    try{
-        const spec=await GalleryPhotoSchema.findOne({_id:req.params});
-        res.status(200).json(spec);
-
-    }
-    catch(error){
-res.status(500).json(error)
-
-    }
-}
+exports.getspecificGalleryPhoto = async (req, res) => {
+  try {
+    const spec = await GalleryPhotoSchema.findOne({ _id: req.params });
+    res.status(200).json(spec);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
